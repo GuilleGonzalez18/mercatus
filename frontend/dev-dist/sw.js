564edee704eb8e974b495a43611b7cdc54903c5e
GuilleGonzalez18 <gonzalezguillermo9818@gmail.com>
2026-05-26 10:53:59 -0300

    New MERCATUS logo

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.s3fqbbl2nok"
+    "revision": "0.jsnilkjf9is"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
